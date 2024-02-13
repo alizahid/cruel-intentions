eac73612d4904e5ba5c11d60e64e80089bd45053
Ali Zahid <ali.zahid@live.com>
2024-02-13 16:59:06 +0500

    Refactoring

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,8 +2,6 @@ import { compact } from 'lodash'
 
 import { type Region } from '~/types'
 
-export const REFETCH_SECRET = process.env.REFETCH_SECRET
-
 export const FACTION = process.env.FACTION ?? 'Horde'
 export const GUILD = process.env.GUILD ?? 'Cruel Intentions'
 export const LEADER_RANK = Number(process.env.LEADER_RANK ?? 1)
