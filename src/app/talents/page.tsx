5015a312a75ab40b129889e2ce3313cd4beb0419
Ali Zahid <ali.zahid@live.com>
2026-08-22 06:36:01 +0500

    Bug fix

--- src/app/talents/page.tsx
+++ src/app/talents/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Suspense } from 'react'
 import { Content } from './content'
 
 export const metadata: Metadata = {
@@ -6,5 +7,9 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return <Content />
+  return (
+    <Suspense>
+      <Content />
+    </Suspense>
+  )
 }
